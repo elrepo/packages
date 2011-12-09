529a95a6a7ae2edf2e292f1a47d1a6362c7a93f6
Philip J Perry <phil@elrepo.org>
2011-12-09 20:23:07 +0800

    ndiswrapper: Fix broken kABI (per_cpu__kstat) in RHEL-6.2
    RHEL-6.2 breaks kABI compatibility for kernel symbol per_cpu__kstat.
    Rebuild against RHEL-6.2 (kernel-2.6.32-220.el6) and add a Requires
    for the RHEL-6.2 kernel.
    
    Signed-off-by: Philip J Perry <phil@elrepo.org>

--- ndiswrapper-kmod/el6/kmodtool-ndiswrapper-el6.sh
+++ ndiswrapper-kmod/el6/kmodtool-ndiswrapper-el6.sh
@@ -130,6 +130,7 @@ get_rpmtemplate ()
     cat <<EOF
 Provides:         kabi-modules = ${verrel}${dotvariant}
 Provides:         ${kmod_name}-kmod = %{?epoch:%{epoch}:}%{version}-%{release}
+Requires:         kernel >= 2.6.32-220.el6
 Requires(post):   /sbin/depmod
 Requires(postun): /sbin/depmod
 EOF
