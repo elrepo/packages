192c3ec01d700c2e6eeef488e3a35b4db233d157
Philip J Perry <phil@elrepo.org>
2014-01-19 15:13:08 +0000

    nct6775: initial import of nct6775 driver
    nct6775 driver imported from https://github.com/groeck/nct6775
    
    Signed-off-by: Philip J Perry <phil@elrepo.org>

---
+++ nct6775-kmod/el6/compat.h
@@ -0,0 +1,216 @@
+#ifndef __COMPAT_H
+#define __COMPAT_H
+
+#include <linux/version.h>
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 16)
+#error This driver is for kernel versions 2.6.16 and later
+#endif
+
+#if !defined (CONFIG_HWMON_VID) && !defined(CONFIG_HWMON_VID_MODULE)
+int vid_from_reg(int val, u8 vrm)
+{
+	return 0;
+}
+
+u8 vid_which_vrm(void)
+{
+	return 0;
+}
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 12, 0)
+static int sysfs_create_groups(struct kobject *kobj,
+			       const struct attribute_group **groups)
+{
+	int error = 0;
+	int i;
+
+	if (!groups)
+		return 0;
+
+	for (i = 0; groups[i]; i++) {
+		error = sysfs_create_group(kobj, groups[i]);
+		if (error) {
+			while (--i >= 0)
+				sysfs_remove_group(kobj, groups[i]);
+			break;
+		}
+	}
+	return error;
+}
+
+static void sysfs_remove_groups(struct kobject *kobj,
+				const struct attribute_group **groups)
+{
+	int i;
+
+	if (!groups)
+		return;
+	for (i = 0; groups[i]; i++)
+		sysfs_remove_group(kobj, groups[i]);
+}
+
+static inline int __must_check PTR_ERR_OR_ZERO(__force const void *ptr)
+{
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+	else
+		return 0;
+}
+
+#endif
+
+#ifdef __NEED_I2C__
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 21)
+#if !(defined RHEL_MAJOR && RHEL_MAJOR == 5 && RHEL_MINOR >= 6)
+/* Simplified version for compatibility */
+struct i2c_board_info {
+	char		type[I2C_NAME_SIZE];
+	unsigned short	flags;
+	unsigned short	addr;
+};
+#endif
+#endif
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 25)
+/* Some older kernels have a different, useless struct i2c_device_id */
+#define i2c_device_id i2c_device_id_compat
+struct i2c_device_id {
+	char name[I2C_NAME_SIZE];
+	kernel_ulong_t driver_data      /* Data private to the driver */
+			__attribute__((aligned(sizeof(kernel_ulong_t))));
+};
+#endif
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 32)
+static unsigned short empty_i2c[] =  { I2C_CLIENT_END };
+static struct i2c_client_address_data addr_data = {
+	.normal_i2c	= normal_i2c,
+	.probe		= empty_i2c,
+	.ignore		= empty_i2c,
+};
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 2, 0)
+static inline s32
+i2c_smbus_read_word_swapped(const struct i2c_client *client, u8 command)
+{
+	s32 value = i2c_smbus_read_word_data(client, command);
+
+	return (value < 0) ? value : swab16(value);
+}
+
+static inline s32
+i2c_smbus_write_word_swapped(const struct i2c_client *client,
+			     u8 command, u16 value)
+{
+	return i2c_smbus_write_word_data(client, command, swab16(value));
+}
+#endif
+
+/* Red Hat EL5 includes backports of these functions, so we can't redefine
+ * our own. */
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 24)
+#if !(defined RHEL_MAJOR && RHEL_MAJOR == 5 && RHEL_MINOR >= 5)
+static inline int strict_strtoul(const char *cp, unsigned int base,
+				 unsigned long *res)
+{
+	*res = simple_strtoul(cp, NULL, base);
+	return 0;
+}
+
+static inline int strict_strtol(const char *cp, unsigned int base, long *res)
+{
+	*res = simple_strtol(cp, NULL, base);
+	return 0;
+}
+#endif
+#endif
+
+#endif	/* __NEED_I2C__ */
+
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 28)
+/*
+ * Divide positive or negative dividend by positive divisor and round
+ * to closest integer. Result is undefined for negative divisors and
+ * for negative dividends if the divisor variable type is unsigned.
+ */
+#define DIV_ROUND_CLOSEST(x, divisor)(			\
+{							\
+	typeof(x) __x = x;				\
+	typeof(divisor) __d = divisor;			\
+	(((typeof(x))-1) > 0 ||				\
+	 ((typeof(divisor))-1) > 0 || (__x) > 0) ?	\
+		(((__x) + ((__d) / 2)) / (__d)) :	\
+		(((__x) - ((__d) / 2)) / (__d));	\
+}							\
+)
+#endif
+
+#ifndef module_driver
+/**
+ * module_driver() - Helper macro for drivers that don't do anything
+ * special in module init/exit. This eliminates a lot of boilerplate.
+ * Each module may only use this macro once, and calling it replaces
+ * module_init() and module_exit().
+ *
+ * @__driver: driver name
+ * @__register: register function for this driver type
+ * @__unregister: unregister function for this driver type
+ * @...: Additional arguments to be passed to __register and __unregister.
+ *
+ * Use this macro to construct bus specific macros for registering
+ * drivers, and do not use it on its own.
+ */
+#define module_driver(__driver, __register, __unregister, ...) \
+static int __init __driver##_init(void) \
+{ \
+	return __register(&(__driver) , ##__VA_ARGS__); \
+} \
+module_init(__driver##_init); \
+static void __exit __driver##_exit(void) \
+{ \
+	__unregister(&(__driver) , ##__VA_ARGS__); \
+} \
+module_exit(__driver##_exit);
+#endif
+
+#ifdef __NEED_I2C__
+
+#ifndef module_i2c_driver
+/**
+ * module_i2c_driver() - Helper macro for registering a I2C driver
+ * @__i2c_driver: i2c_driver struct
+ *
+ * Helper macro for I2C drivers which do not do anything special in module
+ * init/exit. This eliminates a lot of boilerplate. Each module may only
+ * use this macro once, and calling it replaces module_init() and module_exit()
+ */
+#define module_i2c_driver(__i2c_driver) \
+	module_driver(__i2c_driver, i2c_add_driver, \
+			i2c_del_driver)
+#endif
+
+#endif /* __NEED_I2C__ */
+
+#ifndef clamp_val
+#define clamp_val SENSORS_LIMIT
+#endif
+
+#ifndef kstrtol
+#define kstrtol strict_strtol
+#endif
+#ifndef kstrtoul
+#define kstrtoul strict_strtoul
+#endif
+
+#ifndef request_muxed_region
+#define request_muxed_region(a, b, c) (true)
+#endif
+#ifndef release_region
+#define release_region(a, b)
+#endif
+
+#endif /* __COMPAT_H */
---
+++ nct6775-kmod/el6/nct6775-kmod.spec
@@ -0,0 +1,60 @@
+# Define the kmod package name here.
+%define kmod_name nct6775
+
+# If kversion isn't defined on the rpmbuild line, define it here.
+%{!?kversion: %define kversion 2.6.32-431.el6.%{_target_cpu}}
+
+Name:    %{kmod_name}-kmod
+Version: 0.0
+Release: 1.20140118git%{?dist}
+Group:   System Environment/Kernel
+License: GPLv2
+Summary: %{kmod_name} kernel module(s)
+URL:     https://github.com/groeck/nct6775
+
+BuildRequires: redhat-rpm-config
+ExclusiveArch: i686 x86_64
+
+# Sources.
+Source0:  %{kmod_name}-%{version}.tar.bz2
+Source10: kmodtool-%{kmod_name}-el6.sh
+
+# Magic hidden here.
+%{expand:%(sh %{SOURCE10} rpmtemplate %{kmod_name} %{kversion} "")}
+
+# Disable the building of the debug package(s).
+%define debug_package %{nil}
+
+%description
+This package provides the %{kmod_name} kernel module(s).
+It is built to depend upon the specific ABI provided by a range of releases
+of the same variant of the Linux kernel and not on any one specific build.
+
+%prep
+%setup -q -n %{kmod_name}-%{version}
+echo "override %{kmod_name} * weak-updates/%{kmod_name}" > kmod-%{kmod_name}.conf
+
+%build
+KSRC=%{_usrsrc}/kernels/%{kversion}
+%{__make} -C "${KSRC}" %{?_smp_mflags} modules M=$PWD
+
+%install
+export INSTALL_MOD_PATH=%{buildroot}
+export INSTALL_MOD_DIR=extra/%{kmod_name}
+KSRC=%{_usrsrc}/kernels/%{kversion}
+%{__make} -C "${KSRC}" modules_install M=$PWD
+%{__install} -d %{buildroot}%{_sysconfdir}/depmod.d/
+%{__install} kmod-%{kmod_name}.conf %{buildroot}%{_sysconfdir}/depmod.d/
+%{__install} -d %{buildroot}%{_defaultdocdir}/kmod-%{kmod_name}-%{version}/
+%{__install} LICENSE README README.ASRock %{buildroot}%{_defaultdocdir}/kmod-%{kmod_name}-%{version}/
+# Set the module(s) to be executable, so that they will be stripped when packaged.
+find %{buildroot} -type f -name \*.ko -exec %{__chmod} u+x \{\} \;
+# Remove the unrequired files.
+%{__rm} -f %{buildroot}/lib/modules/%{kversion}/modules.*
+
+%clean
+%{__rm} -rf %{buildroot}
+
+%changelog
+* Sat Jan 18 2014 Philip J Perry <phil@elrepo.org> - 0.0-1.20140118git
+- Initial el6 build of the kmod package.
