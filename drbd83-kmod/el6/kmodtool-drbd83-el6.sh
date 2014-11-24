0986bd3e7421e5d1c6dd7a12fd12de4521267961
Philip J Perry <phil@elrepo.org>
2014-11-24 19:58:52 +0000

    drbd83: add dependency for rhel-6.6 kernel
    Signed-off-by: Philip J Perry <phil@elrepo.org>

--- drbd83-kmod/el6/kmodtool-drbd83-el6.sh
+++ drbd83-kmod/el6/kmodtool-drbd83-el6.sh
@@ -133,7 +133,7 @@ Provides:         ${kmod_name}-kmod = %{?epoch:%{epoch}:}%{version}-%{release}
 Requires(post):   /sbin/depmod
 Requires(postun): /sbin/depmod
 
-Requires: kernel >= 2.6.32-279.el6
+Requires: kernel >= 2.6.32-504.el6
 Requires: drbd83-utils >= %{?epoch:%{epoch}:}%{version}
 
 ### We cannot obsolete all Linbit drbd-km-%{kversion} variants
