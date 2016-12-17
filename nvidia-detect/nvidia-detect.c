7da35968a323013cd73f3a4e0e088b472a73a18f
Philip J Perry <phil@elrepo.org>
2016-12-17 15:04:02 +0000

    nvidia-detect: update Xorg ABI for older legacy drivers
    Update XORG_ABI_* for 340 and 304 legacy drivers which now support
    xorg-server 1.19 (Video Driver ABI 23)
    
    Signed-off-by: Philip J Perry <phil@elrepo.org>

--- nvidia-detect/nvidia-detect.c
+++ nvidia-detect/nvidia-detect.c
@@ -61,8 +61,8 @@
 #define XORG_ABI_CURRENT	23	/* 375.20; Xorg 1.19 */
 #define XORG_ABI_96XX		12	/* 96.43.23 */
 #define XORG_ABI_173XX		15	/* 173.14.39 */
-#define XORG_ABI_304XX		20	/* 304.131; Xorg 1.18 */
-#define XORG_ABI_340XX		20	/* 340.96; Xorg 1.18 */
+#define XORG_ABI_304XX		23	/* 304.134; Xorg 1.19 */
+#define XORG_ABI_340XX		23	/* 340.101; Xorg 1.19 */
 
 /* Change the default Xorg log file here if it's different */
 #define XORG_LOG_FILE	"/var/log/Xorg.0.log"
