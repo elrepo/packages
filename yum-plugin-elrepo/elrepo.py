643055e63e9085606a98898fe899c2798c94cc93
Philip J Perry <phil@elrepo.org>
2018-11-18 20:29:00 +0000

    yum-plugin-elrepo: initial commit
    Signed-off-by: Philip J Perry <phil@elrepo.org>

---
+++ yum-plugin-elrepo/elrepo.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+#
+# yum-plugin-elrepo - a yum plugin to remove kmod packages from the
+#                     yum transaction set which require kernels which
+#                     are not yet available.
+#
+# Copyright (C) 2018 Philip J Perry <phil@elrepo.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+from yum.plugins import TYPE_CORE
+import fnmatch
+
+requires_api_version = '2.6'
+plugin_type = (TYPE_CORE,)
+
+def init_hook(conduit):
+    global kernels
+    kernels = []
+
+def exclude_hook(conduit):
+
+    # get installed kernels
+    instpkgs = conduit.getRpmDB().returnPackages()
+    for instpkg in instpkgs:
+        if instpkg.name == "kernel":
+            kernels.append('kernel-modules >= ' + instpkg.version + '-' + instpkg.release + '.' + instpkg.arch)
+            conduit.info(3, '[elrepo]: found installed kernel: %s' % instpkg)
+
+    # get available kernels
+    pkgs = conduit.getPackages()
+    for pkg in pkgs:
+        if pkg.name == "kernel":
+            kernels.append('kernel-modules >= ' + pkg.version + '-' + pkg.release + '.' + pkg.arch)
+            conduit.info(3, '[elrepo]: found kernel: %s' % pkg)
+
+    if not kernels:
+        conduit.info(2, '[elrepo]: ERROR, no kernels found')
+        return
+
+    def find_matches(kmod, requires, matchfor=None):
+
+        # Skip installed packages
+        if kmod.repo.id == "installed":
+            return
+
+        # Skip non-elrepo packages
+        if not (kmod.release).endswith(".elrepo"):
+            return
+
+        # CentOS and SL do not keep older kernels available in their yum repos,
+        # so we can not simply check each kmod for a matching kernel as this would 
+        # exclude kmods built against older kernels no longer available in the repo.
+        # We simply ignore older kmod packages here to prevent them being excluded.
+        if (kmod.release).endswith(".el7.elrepo") or \
+           (kmod.release).endswith(".el7_1.elrepo") or \
+           (kmod.release).endswith(".el7_2.elrepo") or \
+           (kmod.release).endswith(".el7_3.elrepo") or \
+           (kmod.release).endswith(".el7_4.elrepo") or \
+           (kmod.release).endswith(".el7_5.elrepo"):
+             return
+
+        for req in requires:
+            for kernel in kernels:
+                if fnmatch.fnmatch(kernel, req):
+                    return
+        # else no matching kernel, excluding package
+        conduit.info(2, '[elrepo]: excluding package: %s' % kmod)
+        conduit.delPackage(kmod)
+
+        # if kmod-nvidia, handle matching nvidia-x11-drv packages
+        if (kmod.name).startswith("kmod-nvidia"):
+            for pkg in pkgs:
+                if (pkg.name).startswith("nvidia-x11-drv"):
+                    if kmod.version == pkg.version and kmod.release == pkg.release:
+                        conduit.info(2, '[elrepo]: excluding package: %s' % pkg)
+                        conduit.delPackage(pkg)
+
+
+    conduit._base.searchPackageProvides(['kernel-modules >= *', ], callback=find_matches, callback_has_matchfor=True)
---
+++ yum-plugin-elrepo/yum-plugin-elrepo.spec
@@ -0,0 +1,48 @@
+%define pluginhome /usr/lib/yum-plugins
+
+Name:    yum-plugin-elrepo
+Version: 7.5.0
+Release: 1%{?dist}
+Group:   Development/Tools
+License: GPLv2
+Summary: Yum plugin to exclude kmod packages where the required kernel is missing
+URL:     https://github.com/elrepo/packages/tree/master/yum-plugin-elrepo
+
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-build-%(%{__id_u} -n)
+BuildArch: noarch
+
+Source0:   %{name}-%{version}.tar.gz
+
+Requires:	python >= 2.4
+Requires:	yum >= 3.2.22
+
+BuildRequires: python-devel >= 2.4
+
+%description
+A yum plugin to exclude kmod packages from the yum transaction set which
+require kernels that are not yet available.
+
+%prep
+%setup -q
+
+%install
+%{__rm} -rf %{buildroot}
+%{__mkdir_p} %{buildroot}%{_sysconfdir}/yum/pluginconf.d/
+%{__mkdir_p} %{buildroot}%{pluginhome}
+%{__install} -m 0644 elrepo.conf %{buildroot}%{_sysconfdir}/yum/pluginconf.d/
+%{__install} -m 0644 elrepo.py %{buildroot}%{pluginhome}/elrepo.py
+
+%clean
+%{__rm} -rf %{buildroot}
+
+%files
+%defattr(-,root,root)
+%doc COPYING README
+%config(noreplace) %{_sysconfdir}/yum/pluginconf.d/elrepo.conf
+%{pluginhome}/elrepo.py
+%{pluginhome}/elrepo.pyc
+%{pluginhome}/elrepo.pyo
+
+%changelog
+* Sun Nov 18 2018 Philip J Perry <phil@elrepo.org> - 7.5.0-1
+- Initial release of the package
