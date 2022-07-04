b1daf0b3497fe4b74f457ed91832636d9ccc0dc7
Akemi Yagi <amyagi@gmail.com>
2022-07-04 16:31:20 -0700

    kmod-hello-1: build test for el9
    Signed-off-by: Akemi Yagi <amyagi@gmail.com>

---
+++ hello-1-kmod/el9/hello-1.c
@@ -0,0 +1,20 @@
+/*
+* hello-1.c - The simplest kernel module.
+*/
+#include <linux/kernel.h> /* Needed for pr_info() */
+#include <linux/module.h> /* Needed by all modules */
+
+int init_module(void)
+{
+pr_info("Hello world 1.\n");
+
+	/* A non 0 return means init_module failed; module can't be loaded. */
+	return 0;
+}
+
+void cleanup_module(void)
+{
+pr_info("Goodbye world 1.\n");
+}
+
+MODULE_LICENSE("GPL");
